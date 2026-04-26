aeb575203630f9157e22e59a4813c2edc89d9135
Stephanie <200150677+Steph7478@users.noreply.github.com>
2026-04-26 10:41:43 -0300

    fix: main path

--- mobile/build-windows.sh
+++ mobile/build-windows.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-go build -o RemoteControl.exe ../main.go
+go build -o RemoteControl.exe main.go
+
 curl -s -L -o rcedit.exe https://github.com/electron/rcedit/releases/download/v2.0.0/rcedit-x64.exe
 ./rcedit.exe RemoteControl.exe --set-icon assets/icon.ico
+
 echo 'netsh advfirewall firewall add rule name="Remote Control" dir=in action=allow protocol=TCP localport=8080' > install.bat
+echo 'netsh advfirewall firewall delete rule name="Remote Control"' > uninstall.bat
